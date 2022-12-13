96addacf2c2e2a873ba4e2e15bfcd2c4619302a7
cindyhont <cindyhont@gmail.com>
2022-12-13 10:42:52 +0000

    .

--- pages/testpage.tsx
+++ pages/testpage.tsx
@@ -2,30 +2,45 @@ import React, { useEffect } from "react";
 import { GetServerSideProps } from 'next'
 
 export const getServerSideProps: GetServerSideProps = async () => {
-    let result:any = null, link = process.env.SSR_API_URL;
-    try {
-        const 
-            response = await fetch(`${link}/`, {
-                method: 'GET',
-                headers: { 
-                    'Content-Type': 'application/json',
-                },
-            })
-            result = await response.json()
-    } catch (error) {
-        return {props:{result,link}}
-    }
+    let 
+        // result:any = null, 
+        link = process.env.SSR_API_URL.substring(0);
+    // try {
+    //     const 
+    //         response = await fetch(`${link}/`, {
+    //             method: 'GET',
+    //             headers: { 
+    //                 'Content-Type': 'application/json',
+    //             },
+    //         })
+    //         result = await response.json()
+    // } catch (error) {
+    //     return {props:{
+    //         result,
+    //         link
+    //     }}
+    // }
 
-    return {props:{result,link}}
+    return {props:{
+        // result,
+        link
+    }}
 }
 
-const Testpage = ({result,link}:{result:any;link:string;}) => {
-    useEffect(()=>console.log('result:',result),[result])
+const Testpage = (
+    {
+        // result,
+        link
+    }:{
+        // result:any;
+        link:string;
+    }
+) => {
+    // useEffect(()=>console.log('result:',result),[result])
     useEffect(()=>console.log('link:',link),[link])
 
     return (
         <>
-        <pre>{result}</pre>
         <pre>{link}</pre>
         </>
     )
