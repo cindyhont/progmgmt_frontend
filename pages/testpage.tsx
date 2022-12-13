f990b6afcaff9c0b622aea08312ee7875051fb7e
cindyhont <cindyhont@gmail.com>
2022-12-13 09:57:59 +0000

    .

---
+++ pages/testpage.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { GetServerSideProps } from 'next'
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    let result:any = null;
+    try {
+        const 
+            response = await fetch(`${process.env.SSR_API_URL}/`, {
+                method: 'GET',
+                headers: { 
+                    'Content-Type': 'application/json',
+                },
+            })
+            result = await response.json()
+    } catch (error) {
+        return {props:{result}}
+    }
+
+    return {props:{result}}
+}
+
+const Testpage = ({result}:{result:any}) => {
+    useEffect(()=>console.log(result),[])
+    
+    return <pre>{result}</pre>
+}
+
+export default Testpage
