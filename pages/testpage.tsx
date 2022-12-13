8f5ddb6a4781f81d2def9b87673f8234467bc286
cindyhont <cindyhont@gmail.com>
2022-12-13 11:29:42 +0000

    .

--- pages/testpage.tsx
+++ pages/testpage.tsx
@@ -3,44 +3,45 @@ import { GetServerSideProps } from 'next'
 
 export const getServerSideProps: GetServerSideProps = async () => {
     let 
-        // result:any = null, 
+        result:any = null, 
         link = process.env.NEXT_PUBLIC_SSR_API_URL;
-    // try {
-    //     const 
-    //         response = await fetch(`${link}/`, {
-    //             method: 'GET',
-    //             headers: { 
-    //                 'Content-Type': 'application/json',
-    //             },
-    //         })
-    //         result = await response.json()
-    // } catch (error) {
-    //     return {props:{
-    //         result,
-    //         link
-    //     }}
-    // }
+    try {
+        const 
+            response = await fetch(`${link}/`, {
+                method: 'GET',
+                headers: { 
+                    'Content-Type': 'application/json',
+                },
+            })
+            result = await response.json()
+    } catch (error) {
+        return {props:{
+            result,
+            link
+        }}
+    }
 
     return {props:{
-        // result,
+        result,
         link
     }}
 }
 
 const Testpage = (
     {
-        // result,
+        result,
         link
     }:{
-        // result:any;
+        result:any;
         link:string;
     }
 ) => {
-    // useEffect(()=>console.log('result:',result),[result])
+    useEffect(()=>console.log('result:',result),[result])
     useEffect(()=>console.log('link:',link),[link])
 
     return (
         <>
+        <pre>{JSON.stringify(result)}</pre>
         <pre>{link}</pre>
         </>
     )
