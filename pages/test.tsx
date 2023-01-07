cc1c9cc137b479f0bdb9fdf2cfbf6589e641aabc
cindyhont <cindyhont@gmail.com>
2023-01-07 20:09:25 +0000

    .

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,45 +1,53 @@
 import React, { useEffect } from 'react';
 import { GetServerSideProps } from 'next'
 
-const 
-    testLink = (url:string) => new Promise<any>(resolve=>{
-        fetch(url,{
-            headers: { 
-                'Content-Type': 'application/json',
-            },
-        })
-        .then(res=>{
-            res.json()
-        })
-        .then(r=>resolve(r))
-        .catch(e=>resolve(e))
-    })
-
-export const getServerSideProps: GetServerSideProps = async ({req}) => {
+export const getServerSideProps: GetServerSideProps = async () => {
     let links:any = null
+    let resultA:any = null
+    let resultB:any = null
     try {
+        const 
+            testLink = (url:string) => new Promise<any>(resolve=>{
+                fetch(url,{
+                    headers: { 
+                        'Content-Type': 'application/json',
+                    },
+                })
+                .then(res=>{
+                    res.json()
+                })
+                .then(r=>resolve(r))
+                .catch(e=>resolve(e))
+            })
         links = await Promise.all([
             testLink(process.env.NEXT_PUBLIC_SSR_API_URL_A),
             testLink(process.env.NEXT_PUBLIC_SSR_API_URL_B),
         ])
+
+        const responseA = await fetch(process.env.NEXT_PUBLIC_SSR_API_URL_B)
+        resultA = await responseA.json()
+
+        const responseB = await fetch(process.env.NEXT_PUBLIC_SSR_API_URL_B)
+        resultB = await responseB.json()
     } catch {}
 
     return {
         props:{
-            headers:req.headers,
             apiA:process.env.NEXT_PUBLIC_SSR_API_URL_A,
             apiB:process.env.NEXT_PUBLIC_SSR_API_URL_B,
-            links
+            links,
+            resultA
         }
     }
 }
 
-const TestPage = ({headers,apiA,apiB,links}:{headers:any;apiA:string;apiB:string;links:any}) => {
+const TestPage = ({apiA,apiB,links,resultA,resultB}:{apiA:string;apiB:string;links:any;resultA:any;resultB:any;}) => {
     useEffect(()=>{
-        console.log(headers)
         console.log(apiA)
         console.log(apiB)
         console.log(links)
+        console.log(resultA)
+        console.log(resultB)
     },[])
     return <></>
 }
