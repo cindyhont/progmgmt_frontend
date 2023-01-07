cc7c578d69552e2a0303983ff0350415e8820f5e
cindyhont <cindyhont@gmail.com>
2023-01-07 19:12:55 +0000

    .

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,18 +2,30 @@ import React, { useEffect } from 'react';
 import { GetServerSideProps } from 'next'
 
 export const getServerSideProps: GetServerSideProps = async ({req}) => {
+    let json:any = null
+    try {
+        const response = await fetch(process.env.NEXT_PUBLIC_SSR_API_URL, {
+            headers: { 
+                'Content-Type': 'application/json',
+            },
+        })
+        json = await response.json()
+    } catch {}
+
     return {
         props:{
             headers:req.headers,
-            api:process.env.NEXT_PUBLIC_SSR_API_URL
+            api:process.env.NEXT_PUBLIC_SSR_API_URL,
+            json
         }
     }
 }
 
-const TestPage = ({headers,api}:{headers:any;api:string;}) => {
+const TestPage = ({headers,api,json}:{headers:any;api:string;json:any}) => {
     useEffect(()=>{
         console.log(headers)
         console.log(api)
+        console.log(json)
     },[])
     return <></>
 }
